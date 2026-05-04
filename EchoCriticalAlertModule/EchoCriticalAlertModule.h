81188370aaea0c83ac6e76df21da33f4a3eeaf67
gghong15538-wq <gghong15538@gmail.com>
2026-05-04 12:36:38 +0800

    Refactor EchoCriticalAlertModule to conform to WXModuleProtocol

--- EchoCriticalAlertModule/EchoCriticalAlertModule.h
+++ EchoCriticalAlertModule/EchoCriticalAlertModule.h
@@ -1,9 +1,9 @@
 #import <Foundation/Foundation.h>
-#import "DCUniModule.h"
+#import <DCUniBase/DCUniBase.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface EchoCriticalAlertModule : DCUniModule
+@interface EchoCriticalAlertModule : NSObject <WXModuleProtocol>
 
 @end
 
